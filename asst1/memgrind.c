acdb0b9ba0d50c3e924ec87a5f05e644009198dc
sjr204 <56483917+sjr204@users.noreply.github.com>
2019-10-19 22:20:17 -0400

    Separate workloads into functions
    Putting each workload into its own function and defined NUM_WORKLOADS for modularity.
    In progress.

--- asst1/memgrind.c
+++ asst1/memgrind.c
@@ -1,38 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#define NUM_WORKLOADS 100
 
 // Memgrind: a series of workloads to test our mymalloc and myfree implementations.
-// Each workload will be run 100 times, one after the other.
+// Each workload will be run NUM_WORKLOADS times, one after the other.
+
+double workloadA(double *, struct timeval, struct timeval);
+double workloadB(double *, struct timeval, struct timeval);
+double workloadC(double *, struct timeval, struct timeval);
+double workloadD(double *, struct timeval, struct timeval);
+double workloadE(double *, struct timeval, struct timeval);
+double workloadF(double *, struct timeval, struct timeval);
+void calculateRuntime(double* runtime, struct timeval, struct timeval);
+
+void calculateRuntime(double* runtime, struct timeval startTime, struct timeval endTime) {
+    *runtime += ((endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1000000.0);
+}
+// Workload A: malloc() 1 byte and immediately free it - do this 150 times
+double workloadA(double *runtime, struct timeval startTime, struct timeval endTime)
+{
+    int i;
+    gettimeofday(&startTime, NULL);
+    for (i = 0; i < 150; i++)
+    {
+        void *ptr = (void *)malloc(1);
+        if (ptr != NULL)
+        {
+            free(ptr);
+        }
+    }
+    gettimeofday(&endTime, NULL);
+    calculateRuntime(&runtime, startTime, endTime);
+    return *runtime;
+}
+
+double workloadB(double *runtime, struct timeval startTime, struct timeval endTime)
+{
+}
+
+double workloadC(double *runtime, struct timeval startTime, struct timeval endTime)
+{
+}
+
+double workloadD(double *runtime, struct timeval startTime, struct timeval endTime)
+{
+}
+
+double workloadE(double *runtime, struct timeval startTime, struct timeval endTime)
+{
+}
+
+double workloadF(double *runtime, struct timeval startTime, struct timeval endTime)
+{
+}
 
 int main(int argc, char **argv)
 {
-    struct timeval startTime;
-    struct timeval endTime;
-
-    // Workload A: malloc() 1 byte and immediately free it - do this 150 times
-    double runtimeA = 0.0;
+    struct timeval startTime, endTime;
+    double runtimeA, runtimeB, runtimeC, runtimeD, runtimeE, runtimeF;
+    runtimeA = runtimeB = runtimeC = runtimeD = runtimeE = runtimeF = 0;
     int i, j, k;
-    for (i = 0; i < 100; i++)
+
+    for (i = 0; i < NUM_WORKLOADS; i++)
     {
-        gettimeofday(&startTime, NULL);
-        for (j = 0; j < 150; j++)
-        {
-            void *ptr = (void *)malloc(1);
-            if (ptr != NULL)
-            {
-                free(ptr);
-            }
-        }
-        gettimeofday(&endTime, NULL);
-        runtimeA += ((endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1000000.0);
+        workloadA(&runtimeA, startTime, endTime);
     }
-    runtimeA = (runtimeA / 100); // Calculate mean runtime of workload A
+    runtimeA = runtimeA / NUM_WORKLOADS;
 
     // Workload B: malloc() 1 byte, store the pointer in an array - do this 150 times.
     // Once you've malloc()ed 50 byte chunks, then free() the 50 1 byte pointers one by one.
-    double runtimeB = 0.0;
-    for (i = 0; i < 100; i++)
+    for (i = 0; i < NUM_WORKLOADS; i++)
     {
         gettimeofday(&startTime, NULL);
         void *ptrArr[50];
@@ -41,13 +79,15 @@ int main(int argc, char **argv)
             for (k = 0; k < 50; k++)
             {
                 void *ptr = (void *)malloc(1);
-                if (ptr != NULL) {
+                if (ptr != NULL)
+                {
                     ptrArr[k] = ptr;
                 }
             }
             for (k = 0; k < 50; k++)
             {
-                if (ptrArr[k] != NULL) {
+                if (ptrArr[k] != NULL)
+                {
                     free(ptrArr[k]);
                 }
             }
@@ -55,7 +95,7 @@ int main(int argc, char **argv)
         gettimeofday(&endTime, NULL);
         runtimeB += ((endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1000000.0);
     }
-    runtimeB = (runtimeB / 100);
+    runtimeB = (runtimeB / NUM_WORKLOADS);
 
     // Workload C: Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
     //      > do this until you have allocated 50 times
@@ -63,8 +103,7 @@ int main(int argc, char **argv)
     //      > if you have already allocated 50 times, disregard the random and just free() on each iteration
     // - Keep track of each operation so that you eventually free() all pointers
     //      > don't allow a free() if you have no pointers to free()
-    double runtimeC = 0.0;
-    for (i = 0; i < 100; i++)
+    for (i = 0; i < NUM_WORKLOADS; i++)
     {
         gettimeofday(&startTime, NULL);
         int counter = 0;             // Total amount of times malloc() has been called.
@@ -101,22 +140,22 @@ int main(int argc, char **argv)
         while (remainingPtrs > 0)
         { // Free rest of memory.
             remainingPtrs--;
-            if (ptrArr[remainingPtrs] != NULL) {
+            if (ptrArr[remainingPtrs] != NULL)
+            {
                 free(ptrArr[remainingPtrs]);
             }
         }
         gettimeofday(&endTime, NULL);
         runtimeC += ((endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1000000.0);
     }
-    runtimeC = (runtimeC / 100);
+    runtimeC = (runtimeC / NUM_WORKLOADS);
 
     // Workload D: Randomly choose between a randomly-sized malloc() or free()ing a pointer – do this many times (see below)
     // Keep track of each malloc so that all mallocs do not exceed your total memory capacity
     // Keep track of each operation so that you eventually malloc() 50 times
     // Keep track of each operation so that you eventually free() all pointers
     // Choose a random allocation size between 1 and 64 bytes
-    double runtimeD = 0.0;
-    for (i = 0; i < 100; i++)
+    for (i = 0; i < NUM_WORKLOADS; i++)
     {
         gettimeofday(&startTime, NULL);
         int counter = 0;             // Total amount of times malloc() has been called.
@@ -167,21 +206,19 @@ int main(int argc, char **argv)
         gettimeofday(&endTime, NULL);
         runtimeD += ((endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1000000.0); // Add runtime of current workload to total runtime
     }
-    runtimeD = (runtimeD / 100); // Mean runtime of 100 workloads
+    runtimeD = (runtimeD / NUM_WORKLOADS); // Mean runtime of NUM_WORKLOADS workloads
 
     // Workload E
-    double runtimeE = 0.0;
-    for (i = 0; i < 100; i++)
+    for (i = 0; i < NUM_WORKLOADS; i++)
     {
     }
-    runtimeE = (runtimeE / 100); // Mean runtime of 100 workloads
+    runtimeE = (runtimeE / NUM_WORKLOADS); // Mean runtime of NUM_WORKLOADS workloads
 
     // Workload F
-    double runtimeF = 0.0;
-    for (i = 0; i < 100; i++)
+    for (i = 0; i < NUM_WORKLOADS; i++)
     {
     }
-    runtimeF = (runtimeF / 100); // Mean runtime of 100 workloads
+    runtimeF = (runtimeF / NUM_WORKLOADS); // Mean runtime of NUM_WORKLOADS workloads
 
     printf("Mean runtime for workload A: %0.6f seconds\n", runtimeA);
     printf("Mean runtime for workload B: %0.6f seconds\n", runtimeB);
